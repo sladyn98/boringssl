4fe63361217749331767fe0d644134a9f4d272b2
Sladyn <sladynnunes98@gmail.com>
2019-06-25 23:32:30 +0530

    Update base.h
    Added support for s390x system

--- include/openssl/base.h
+++ include/openssl/base.h
@@ -114,6 +114,9 @@ extern "C" {
 #define OPENSSL_32_BIT
 #elif defined(__myriad2__)
 #define OPENSSL_32_BIT
+#elif defined(__s390x__)
+#define OPENSSL_64_BIT
+#define OPENSSL_S390X
 #else
 // Note BoringSSL only supports standard 32-bit and 64-bit two's-complement,
 // little-endian architectures. Functions will not produce the correct answer

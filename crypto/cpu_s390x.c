70a9538cc6869f9f689696fd41d88330d6eb099d
Sladyn <gunnerforlife00@gmail.com>
2019-07-18 01:25:06 +0530

    added dlopen() function to the is_s390x_capabale() callback.

--- crypto/cpu_s390x.c
+++ crypto/cpu_s390x.c
@@ -1,7 +1,20 @@
 
 #include <openssl/base.h>
-#include "../../internal.h";
+#include "../../internal.h"
+#include <stdio.h>
+#include <map>
+#include <stdlib.h>
+#include <dlfcn.h>
+
+
+
 
 int is_s390x_capable(){
-    return 1;
+
+    void *handle;
+    handle = dlopen("filename",RTLD_NOW);
+     if (!handle) {
+        fprintf(stderr, "%s\n", dlerror());
+        exit(EXIT_FAILURE);
+    }
 }

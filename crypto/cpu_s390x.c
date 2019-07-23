f5f1339905861a2835dbfda18037cb106dfcc336
Sladyn <gunnerforlife00@gmail.com>
2019-07-23 22:34:05 +0530

    Added outline for symbol storage

--- crypto/cpu_s390x.c
+++ crypto/cpu_s390x.c
@@ -12,9 +12,25 @@
 int is_s390x_capable(){
 
     void *handle;
+    int  *iptr;
+    int *pointerArray[100]; // To be used to store the addresses of the symbols.
     handle = dlopen("filename",RTLD_NOW);
      if (!handle) {
         fprintf(stderr, "%s\n", dlerror());
         exit(EXIT_FAILURE);
     }
+    
+}
+
+
+int xxx_yyy(char *x, int y, void *handle)
+{
+	static char *epName = "ica_xxx_yyy";
+	static int (*ica_xxx_yyy)(char *, int) = NULL;
+	
+	if (ica_xxx_yyy == NULL) {
+		ica_xxx_yyy = dlsym(handle, epName);
+	}
+	
+	return (*(ica_xxx_yyy)(x, y));
 }
